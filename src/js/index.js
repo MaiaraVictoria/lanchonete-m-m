a6165ae232e6afca0e2f68f2fde486959b773df7
Maiara Victoria <126670392+MaiaraVictoria@users.noreply.github.com>
2023-03-16 14:07:03 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+alert('AVISO!! Imagens fortes a seguir!');
+
+const botaoCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+// BURGUINHO
+
+botaoCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.selecionado');
+
+        
+        botaoSelecionado.classList.remove('selecionado');
+
+        botao.classList.add('selecionado');
+
+        const imagemAtiva = document.querySelector('.ativa');
+
+        imagemAtiva.classList.remove('ativa');
+          
+        
+         
+        imagens[indice].classList.add('ativa');
+        
+
+    });
+})
